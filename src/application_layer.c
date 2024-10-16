0ccb785d4f9492e11eb4aabd3465f2a0447da323
Teresa Mascarenhas <115081350+teresaam7@users.noreply.github.com>
2024-10-17 00:46:15 +0100

    Correction in application

--- src/application_layer.c
+++ src/application_layer.c
@@ -3,6 +3,10 @@
 #include "application_layer.h"
 #include "link_layer.h"
 
+unsigned char* createCtrlPacket(int c, int* fileSize, const char *filename);
+unsigned char* createDataPacket(int sequence, int dataSize, unsigned char* data);
+unsigned char* parseCtrlPacket(unsigned char* packet, int size);
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
@@ -22,58 +26,58 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         perror("Connection error\n");
         exit(-1);
     }
-    
+
 //LlTx abre o ficheiro lê o que tem dentro e separa os dados em packets e dps manda os packets para o Rx  
 
-    switch(linkLayer.role) {
+     switch(linkLayer.role) {
         case LlTx: {    ////////////////////(slide 27 para construir estes packets)/////////////////////////
-            //1. cria um control packet no início para dizer que começou a transmissão (c=1)
-            //2. cria data packets enquanto houver data. Acho que o tamanho destes data packets deve ser o MAX_PAYLOAD_SIZE definido no ll.h
-            //3. cria um control packet no fim para dizer que acabou a transmissão (c=3) 
+            // 1. cria um control packet no início para dizer que começou a transmissão (c=1)
+            // 2. cria data packets enquanto houver data. Acho que o tamanho destes data packets deve ser o MAX_PAYLOAD_SIZE definido no ll.h
+            // 3. cria um control packet no fim para dizer que acabou a transmissão (c=3) 
             FILE* file = fopen(filename, "rb");
             if (file == NULL) {
                 perror("File doesn't exist\n");
                 exit(-1);
             }
             fseek(file, 0L, SEEK_END); // https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
-            long fileSize = ftell(file);//
+            int fileSize = ftell(file); // Get file size
+            fseek(file, 0L, SEEK_SET); // Reset file position to beginning for reading
 
-            unsigned char* ctrlPacket = createCtrlPacket(1, &fileSize, &filename); 
-            int ctrlPacketSize = 5 + sizeof(fileSize) + sizeof(filename); // c+ T1 +T2 +V1 + V2
-            if(llwrite(ctrlPacket, ctrlPacketSize) == -1){
-                    perror("Error in ctrlPacket \n");
-                    exit(-1);
-                }
+            unsigned char* ctrlPacket = createCtrlPacket(1, &fileSize, filename); 
+            int ctrlPacketSize = 5 + sizeof(fileSize) + strlen(filename); // c + T1 + T2 + V1 + V2
+            if(llwrite(ctrlPacket, ctrlPacketSize) == -1) {
+                perror("Error in ctrlPacket \n");
+                exit(-1);
+            }
 
             int dataPacketCount = 0;
             int nDataPackets = (fileSize + MAX_PAYLOAD_SIZE - 1) / MAX_PAYLOAD_SIZE;
 
             for (int i = 0; i < nDataPackets; ++i) {
-            int dataSize;
-            if (i == nDataPackets - 1) {
-                dataSize = fileSize % MAX_PAYLOAD_SIZE;
+                int dataSize = (i == nDataPackets - 1) ? (fileSize % MAX_PAYLOAD_SIZE) : MAX_PAYLOAD_SIZE;
                 if (dataSize == 0) dataSize = MAX_PAYLOAD_SIZE;
-                else dataSize = MAX_PAYLOAD_SIZE;
 
                 unsigned char* data = (unsigned char*) malloc(dataSize);
-                int dataPacketSize = 4 + dataSize;
+                fread(data, sizeof(unsigned char), dataSize, file); // Read data from file
+
                 unsigned char* dataPacket = createDataPacket(dataPacketCount, dataSize, data);
-                if(llwrite(dataPacket, dataPacketSize) == -1){
+                if(llwrite(dataPacket, 4 + dataSize) == -1) {
                     perror("Error in dataPacket \n");
+                    free(data); // Free data after use
                     exit(-1);
                 }
-            dataPacketCount++;
+                dataPacketCount++;
+                free(data); // Free allocated memory
             }
 
-            unsigned char* ctrlPacket = createCtrlPacket(3, &fileSize, &filename);
-            if(llwrite(ctrlPacket, ctrlPacketSize) == -1){
-                    perror("Error in ctrlPacket \n");
-                    exit(-1);
-                }
-
+            unsigned char* endCtrlPacket = createCtrlPacket(3, &fileSize, filename);
+            if(llwrite(endCtrlPacket, ctrlPacketSize) == -1) {
+                perror("Error in final ctrlPacket \n");
+                exit(-1);
+            }
 
             fclose(file); 
-            if (llclose(fd)<0) {
+            if (llclose(fd) < 0) {
                 perror("Closing error\n");
                 exit(-1);
             }
@@ -88,7 +92,6 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
 
             int packetSize = -1;
 
-
             while ((packetSize = llread(fd, packet)) < 0);
             if (packetSize < 0) {
                 fprintf(stderr, "Error reading control packet\n");
@@ -96,7 +99,6 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
                 return EXIT_FAILURE;
             }
 
-            unsigned long int rxFileSize = 0;
             unsigned char* fileName = parseCtrlPacket(packet, packetSize);
             if (fileName == NULL) {
                 fprintf(stderr, "Failed to parse control packet\n");
@@ -118,33 +120,18 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
                 }
 
                 if (packet[0] == 2) { 
-                    unsigned char *buffer = (unsigned char *)malloc(packetSize);
-                    if (buffer == NULL) {
-                        perror("Failed to allocate memory for buffer");
-                        fclose(newFile); 
-                        free(packet); 
-                        return EXIT_FAILURE;
-                    }
-
+                    unsigned char *buffer = (unsigned char *)malloc(packetSize - 4);
                     memcpy(buffer, packet + 4, packetSize - 4);
-                    buffer += packetSize + 4;
                     fwrite(buffer, sizeof(unsigned char), packetSize - 4, newFile); 
                     free(buffer); 
                 }
             }
             
-                fclose(newFile);
-                free(packet);
+            fclose(newFile);
+            free(packet);
 
-                    //buscar o nome do file ao packet de controlo lido
-                    //read data e escrever num file
-                    //read ctrl final
-
-
-                    break;
-            }
+            break;
         }
-
     }
 }
 
@@ -163,7 +150,11 @@ unsigned char * parseCtrlPacket(unsigned char* packet, int size){
 }
 
 unsigned char* createCtrlPacket(int c, int* fileSize, const char *filename){
-    unsigned char* packet;
+    unsigned char* packet = (unsigned char*)malloc(5 + sizeof(*fileSize) + strlen(filename)); // Allocate memory
+    if (!packet) {
+        perror("Failed to allocate memory for control packet");
+        return NULL;
+    }
     //ver slide 27
     packet[0] = c;
     int i = 1;
@@ -173,6 +164,7 @@ unsigned char* createCtrlPacket(int c, int* fileSize, const char *filename){
     packet[i++] = 1;
     packet[i++] = sizeof(filename);
     packet[i++] = filename;
+    //memcpy(packet + i, filename, strlen(filename)); 
 
     //T é tipo se é 0, 1, 2...
     //L -> tamanho de cada cena por exemplo se o T é 0, L vai ter o tamanho do fileSize
@@ -181,7 +173,7 @@ unsigned char* createCtrlPacket(int c, int* fileSize, const char *filename){
     return packet;
 }
 unsigned char* createDataPacket(int sequence, int dataSize, unsigned char* data){
-    unsigned char* packet;
+    unsigned char* packet = (unsigned char*)malloc(4 + dataSize); 
     //ver slide 27
     packet[0] = 2;
     packet[1] = sequence;
@@ -190,3 +182,6 @@ unsigned char* createDataPacket(int sequence, int dataSize, unsigned char* data)
     memcpy(packet + 4, data, dataSize);
     return packet;
 }
+
+
+
