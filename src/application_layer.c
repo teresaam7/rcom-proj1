dee595f6a6206741ccf08ef24400e6b2ec763122
dimochii <diana.mm.nunes.28@gmail.com>
2024-10-16 17:33:55 +0100

    Tx done?

--- src/application_layer.c
+++ src/application_layer.c
@@ -23,7 +23,7 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         exit(-1);
     }
 //LlTx abre o ficheiro lê o que tem dentro e separa os dados em packets e dps manda os packets para o Rx  
-//LlRx recebe os packets reconstroi o ficheiro e escreve o no disco
+
     switch(linkLayer.role) {
         case LlTx: {    ////////////////////(slide 27 para construir estes packets)/////////////////////////
             //1. cria um control packet no início para dizer que começou a transmissão (c=1)
@@ -37,25 +37,34 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
             fseek(file, 0L, SEEK_END); // https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
             long fileSize = ftell(file);//
 
-            unsigned char* ctrlPacket = createCtrlPacket(1); 
+            unsigned char* ctrlPacket = createCtrlPacket(1, &fileSize, &filename); 
+            int ctrlPacketSize = 5 + sizeof(fileSize) + sizeof(filename); // c+ T1 +T2 +V1 + V2
             if(llwrite(ctrlPacket, ctrlPacketSize) == -1){
                     perror("Error in ctrlPacket \n");
                     exit(-1);
                 }
 
-            //vamos andar de M_P_S em M_P_S entao secalhar fazia dataP = 0 while(dataP <fileSize/M_P_S) dataP ++ ?
             int dataPacketCount = 0;
-            int nDataPackets = fileSize/MAX_PAYLOAD_SIZE;
+            int nDataPackets = (fileSize + MAX_PAYLOAD_SIZE - 1) / MAX_PAYLOAD_SIZE;
 
-            while(dataPacketCount < nDataPackets){
-                unsigned char* dataPacket = createDataPacket();
+            for (int i = 0; i < nDataPackets; ++i) {
+            int dataSize;
+            if (i == nDataPackets - 1) {
+                dataSize = fileSize % MAX_PAYLOAD_SIZE;
+                if (dataSize == 0) dataSize = MAX_PAYLOAD_SIZE;
+                else dataSize = MAX_PAYLOAD_SIZE;
+
+                unsigned char* data = (unsigned char*) malloc(dataSize);
+                int dataPacketSize = 4 + dataSize;
+                unsigned char* dataPacket = createDataPacket(dataPacketCount, dataSize, data);
                 if(llwrite(dataPacket, dataPacketSize) == -1){
                     perror("Error in dataPacket \n");
                     exit(-1);
                 }
+            dataPacketCount++;
             }
 
-            unsigned char* ctrlPacket = createCtrlPacket(3);
+            unsigned char* ctrlPacket = createCtrlPacket(3, &fileSize, &filename);
             if(llwrite(ctrlPacket, ctrlPacketSize) == -1){
                     perror("Error in ctrlPacket \n");
                     exit(-1);
@@ -70,24 +79,45 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
             break;
         }
         case LlRx: {
+            //read ctrl
+            unsigned char *packet = (unsigned char *)malloc(MAX_PAYLOAD_SIZE);
+            while(llread(packet))
+            //read data e escrever num file
+            //read ctrl final
 
-
-
+            
             break;
+            }
         }
+
     }
-
-
 }
 
-
-unsigned char* createCtrlPacket(int c){
+unsigned char* createCtrlPacket(int c, int* fileSize, const char *filename){
     unsigned char* packet;
     //ver slide 27
+    packet[0] = c;
+    int i = 1;
+    packet[i++] = 0; //file size
+    packet[i++] = sizeof(fileSize);
+    packet[i++] = fileSize;
+    packet[i++] = 1;
+    packet[i++] = sizeof(filename);
+    packet[i++] = filename;
+
+    //T é tipo se é 0, 1, 2...
+    //L -> tamanho de cada cena por exemplo se o T é 0, L vai ter o tamanho do fileSize
+    //V -> actual fileSize
+
     return packet;
 }
-unsigned char* createDataPacket(){
+unsigned char* createDataPacket(int sequence, int dataSize, unsigned char* data){
     unsigned char* packet;
     //ver slide 27
+    packet[0] = 2;
+    packet[1] = sequence;
+    packet[2] = dataSize >> 8 & 0xFF;
+    packet[3] = dataSize & 0xFF;
+    memcpy(packet + 4, data, dataSize);
     return packet;
 }
